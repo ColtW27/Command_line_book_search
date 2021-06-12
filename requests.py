ab13dff848aa93a6a47494f873b15b963d65b59c
Colton Wilmouth <cwilmouth27@yahoo.com>
2021-06-12 13:57:29 -0400

    Add ability to query for books and receive top 5 results

--- requests.py
+++ requests.py
@@ -5,11 +5,16 @@ from urllib.request import urlopen  # Python built-in module for opemning and re
 api = "https://www.googleapis.com/books/v1/volumes?q=search_query:"
 search_queury = input(f"Please enter your search \n ") #.strip()
 
-# send a restful request and receives the response as JSON
+# send a restful request and receives the response as JSON. This is the entire
 response = urlopen(api + search_queury)
 # Allows the parsing and conversion of JSON into Python
-book_data = json.load(response)
+book_search_response = json.load(response)  # Represents the entire object and data on search
+book_items_list = book_search_response["items"] # Items array that holds the volumes (books)
+first_book = book_items_list[0]
+# print(book_data["items"][0]['volumeInfo']["title"])
+# print(first_book)
 
-print(response)
-print(search_queury)
-print(book_data["items"][0]['volumeInfo']["title"])
+# print(first_book)
+for i in range(0,5):
+    # print(i)
+    print(book_items_list[i]['volumeInfo']["title"])
