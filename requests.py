cf6a38753d2ebeec6604231cf5065c3301358e93
Colton Wilmouth <cwilmouth27@yahoo.com>
2021-06-14 15:21:26 -0400

    Add functionality for error handling withing book replacement function

--- requests.py
+++ requests.py
@@ -68,6 +68,24 @@ def print_reading_list(reading_list):  # Prints the user's reding list
         print(f"({count + 1}) {value}")
     print("\n")
 
+def get_list_number_for_book_replacement():
+    book_to_replace = ""
+    while book_to_replace not in range(1, 6):
+        try:
+            book_to_replace = int(input("""Which book would you like \
+to replace? Please select a list number. \n"""))
+        except ValueError:
+            book_to_replace = ""
+            if book_to_replace not in range(1, 6):
+                print("That response is not a number between 1 and 5. Please try again.")
+            # continue
+
+        # if book_to_replace not in range(1, 6):
+        #     print("That response is not a number between 1 and 5. Please try again.")
+    # if book_to_replace not in range(1, 6):
+
+    return int(book_to_replace) - 1
+    
 
 def add_book_to_reading_list(volume):
     volume = int(volume)
@@ -83,8 +101,7 @@ to replace an item in your list? (Yes/No) \n""")
                 print_search_results(search_results)
                 print_reading_list(reading_list)
 
-                book_to_replace = int(input("""Okay, which book would you like \
-to replace? Please select a list number. \n""")) - 1
+                book_to_replace = get_list_number_for_book_replacement()
                 reading_list[book_to_replace] = volume_from_search_results
             elif replace_item == "no":
                 break
@@ -92,24 +109,6 @@ to replace? Please select a list number. \n""")) - 1
                 print("""Sorry, you seem to have entered an invalid response, please reread the prompt for clarification and try again.""")
     print_search_results(search_results)
     print_reading_list(reading_list)
-# def add_book_to_reading_list(volume):
-#     volume = int(volume)
-#     volume_from_search_results = search_results[volume]  # gets the current volume
-#     if len(reading_list) < 5:
-#         reading_list.append(volume_from_search_results)
-#     else:  # at this point the user's reading list is full
-#         replace_item = input("""It looks like your list is full. Would you like \
-# to replace an item in your list? (Yes/No) \n""")
-#         if replace_item == "yes":
-#             print_search_results(search_results)
-#             print_reading_list(reading_list)
-
-#             book_to_replace = int(input("""Okay, which book would you like \
-# to replace? Please select a list number. \n""")) - 1
-#             reading_list[book_to_replace] = volume_from_search_results
-
-#     print_search_results(search_results)
-#     print_reading_list(reading_list)
 
       
 still_searching = "yes"
