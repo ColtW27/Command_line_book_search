f3990295415a7bca7db60e9be16a6d8a22533cb7
Colton Wilmouth <cwilmouth27@yahoo.com>
2021-06-14 11:29:57 -0400

    Add necesssary comments, remove unused print statements

--- requests.py
+++ requests.py
@@ -10,18 +10,21 @@ response = urlopen(api + search_queury)
 # Allows the parsing and conversion of JSON into Python
 book_search_response = json.load(response)  # Represents the entire object and data on search
 book_items_list = book_search_response["items"] # Items array that holds the volumes (books)
-first_book = book_items_list[0]
-# print(book_data["items"][0]['volumeInfo']["title"])
-# print(first_book)
 
 
-def get_authors(volume):
-    return volume["volumeInfo"]["authors"]
+def get_authors(volume):  # returns the list of authors for the volume 
+    if "authors" in volume["volumeInfo"]:
+        return volume["volumeInfo"]["authors"]
+    else:
+        return "Unavailable"
 
-def get_title(volume):
-    return volume["volumeInfo"]["title"]
+def get_title(volume):  # returns the title for the volume
+    if "title" in volume["volumeInfo"]:
+        return volume["volumeInfo"]["title"]
+    else:
+        return "Unavailable"
 
-def get_publisher(volume):
+def get_publisher(volume):  # returns the publisher for the volume
     if "publisher" in volume["volumeInfo"]:
         return volume["volumeInfo"]["publisher"]
     else:
@@ -30,7 +33,7 @@ def get_publisher(volume):
 search_results = {}  # create an object to store the seach results so that they 
 # are accessible to add to reading list by id
 
-def create_search_results():
+def create_search_results():  # curates a list of the top 5 search results
     for i in range(0,5):  # author, title, and publishing company.
         list_id = i + 1
         volume = book_items_list[i]
@@ -41,50 +44,47 @@ def create_search_results():
         volume_information = [title, authors, publisher]
         search_results[list_id] = volume_information  # add volume to search results
     #   under a key of id , offset from index to prevent a 0 list position
-        # print(f"({list_id})  {volume_information}")
 
 
-def print_search_results(search_results):
+def print_search_results(search_results):  # prints the search results list
     print("SEARCH RESULTS \n")
     for count, value in enumerate(search_results):
-        list_id = count - 1
+        list_id = count + 1
         volume_information = search_results[value]
-        # print(f"search_results[value]")
         print(f"({list_id})  {volume_information} ")
     print("\n")  
 
-create_search_results()
-print_search_results(search_results)
+create_search_results()  # curates a list of the top 5 search results
+print_search_results(search_results)  # prints the search results list
 
 add_to_reading_list = input(f"""Would you like to add any of these to your \
 reading list?  (Yes/no)""").lower()
 
-def print_reading_list(reading_list):
+def print_reading_list(reading_list):  # Prints the user's reding list
     print("READING LIST \n")
     for count, value in enumerate(reading_list):
-        print(f"{count + 1} {value}")
+        print(f"({count + 1}) {value}")
     print("\n")
 
-reading_list = []  # this can only have 5 items
+reading_list = []  # Holds the user's reading list, this can only have 5 items
 
 
 def add_book_to_reading_list(volume):
     volume = int(volume)
-    volume_from_search_results = search_results[volume]
+    volume_from_search_results = search_results[volume]  # gets the current volume
     if len(reading_list) < 5:
         reading_list.append(volume_from_search_results)
-    else:
+    else:  # at this point the user's reading list is full
         replace_item = input("""It looks like your list is full. Would you like \
 to replace an item in your list? (Yes/No) \n""")
         if replace_item == "yes":
-            # print(reading_list)
             print_search_results(search_results)
             print_reading_list(reading_list)
 
             book_to_replace = int(input("""Okay, which book would you like \
 to replace? Please select a list number. \n""")) - 1
             reading_list[book_to_replace] = volume_from_search_results
-    # print(reading_list)
+
     print_search_results(search_results)
     print_reading_list(reading_list)
 
