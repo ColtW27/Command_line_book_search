ac9985ca85b38a7dd4af53fae734712585647504
Colton Wilmouth <cwilmouth27@yahoo.com>
2021-06-21 18:58:36 -0400

    Add sourced helper function to remove emojies, add encoding

--- clean_search_query.py
+++ clean_search_query.py
@@ -2,11 +2,18 @@
 # -*- coding: utf-8 -*-
 import emoji
 
+
+def remove_emoji(text):  # removes emojis from input, credit in resources for 
+# removing emojies
+    return emoji.get_emoji_regexp().sub(u'', text)
+
 def clean_search(query):  # Takes in a query to remove all white space
+    query = remove_emoji(query)  # helper to remove emojies
     query = query.strip()  # removes outer whitespace
     query = query.split(" ")  # splits into a list to separate out valid words
     query = filter(lambda x: x != "", query)  # filter out space
     query =  "".join(list(query))  # convert back to list from filter and combine
+    
     return query  # return the new query
 
 # Special characters are left in, as they are able to be processed by the search
@@ -22,12 +29,8 @@ def clean_search(query):  # Takes in a query to remove all white space
 # print(clean_search("Dance     !"))  # output => "Dance!"
 # print(clean_search("Dance  ""   !"))  # output => "Dance!"
 # print(clean_search("    Dance ''   on   the street!"))
-# print(clean_search("    Dance    on   the street!"))
+# print(clean_search("    Dance    on   the street!"))  # Danceonthestreet!
+# print(clean_search("    Dance    on  😁 the street!")) # Danceonthestreet!
+# print(clean_search("    Dance    on   the🚨street!"))  # Danceonthestreet!
 
-text = "get the deal 😁"
-text = "get the deal 🚨"
 
-def remove_emoji(text):
-    return emoji.get_emoji_regexp().sub(u'', text)
-
-print(remove_emoji(text))
