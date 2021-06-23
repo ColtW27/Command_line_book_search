bf6c1b92048cede3db3be3e824dbdc491bcd0f70
Colton Wilmouth <cwilmouth27@yahoo.com>
2021-06-23 14:12:21 -0400

    Add sample_test_data file to house test data separately

--- print_lists.py
+++ print_lists.py
@@ -5,7 +5,6 @@ def print_search_results(search_results):  # prints the search results list
     print("SEARCH RESULTS")
     print()
     t = Texttable()  # creates a blank texttable 
-
     for count, value in enumerate(search_results):  # iterates over search results to build out table
         list_id = count + 1  # offset to remove zero index from table 
         title = search_results[value][0]
--- test_print_lists.py
+++ test_print_lists.py
@@ -1,5 +1,12 @@
 import unittest
-
+from print_lists import print_reading_list, print_search_results
+from sample_test_data import test_search_results
 
 class TestPrintListMethods(unittest.TestCase):
     
+    def test_print_reading_list(self):
+        pass
+
+
+    def test_pringt_search_results(self):
+        pass
